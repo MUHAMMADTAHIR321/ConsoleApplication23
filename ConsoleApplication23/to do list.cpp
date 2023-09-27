df438b31e9ef6887525eb335c4a62d491eb0ab11
TAHIR ZAHEER <tahirzaheer121@gmail.com>
2023-09-27 18:22:04 +0500

    update

--- ConsoleApplication23/to do list.cpp
+++ ConsoleApplication23/to do list.cpp
@@ -1,39 +1,79 @@
 #include <iostream>
+# include<string>
+# include<vector>
+
 using namespace std;
-int main()
-{
-	do {
+
+	void showmenu() {
 		cout << "TO DO LIST\n";
 		cout << "MENU\n";
 		cout << "1. Add Task\n";
 		cout << "2. Display List of Task\n";
 		cout << "3. Mark Task as Completed\n";
-		cout << "4. Remove Task\n";
-		cout << "5. Quit App\n";
+		cout << "4. Quit App\n";
+		
+	}
+	int main()
+	{
+		vector<string>tasks;
+		vector<bool>taskStatus;
 
-		int choice;
-		cout << "Enter your Choice";
-		cin >> choice;
+		while (true) {
+			showmenu();
 
-		switch (choice) {
-		case 1:
-			cout << "Enter Task Discription";
-			break;
-		case 2:
-			cout << "Task List";
-			break;
-		case 3:
-			cout << "Mark Task as Completed";
-			break;
-		case 4:
-				cout << "Good Bye";
-			break;
-		default:
-			cout << "Invalid Choice.Please try again";
+			int choice;
+			cout << "Enter your Choice";
+			cin >> choice;
+
+			switch (choice) {
+			case 1: {
+				cout << "Enter a new Task";
+
+				string task;
+				cin.ignore();
+				getline(cin, task);
+				tasks.push_back(task);
+				taskStatus.push_back(false);
+				break;
+			}
+
+			case 2: {
+				cout << "To do List";
+				for (int i = 0; i < tasks.size();i++) {
+					cout << (i + 1) << ". ";
+					if (taskStatus[i]) {
+						cout << "[Done] ";
+					}
+					cout << tasks[i] << endl;
+					break;
+
+				}
+			case 3: {
+				cout << "Enter the task number to mark as done: ";
+				int taskNum;
+				cin >> taskNum;
+
+				if (taskNum >= 1 && taskNum <= tasks.size()) {
+					taskStatus[taskNum - 1] = true;
+					cout << "Task marked as done!" << endl;
+				}
+				else {
+					cout << "Invalid task number." << endl;
+				}break;
+			}
+
+
+			case 4:
+			{cout << "Good Bye";
+			break;}
+			default:
+				cout << "Invalid Choice.Please try again";
+			}
+
+
+			}
 		}
 
-
-
-
 			return 0;
-	}
+		}
+	
